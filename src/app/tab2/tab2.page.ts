e82279ac9f95a76d26b66180207c14f0e2060e7a
Miguel Arciniega <milearciniegara@ittepic.edu.mx>
2021-01-12 03:31:41 -0700

    Reparación de Detalles

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Estudiante } from '../models/estudiante';
+import { EstudianteService } from '../services/estudiante.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public students: Estudiante[];
 
+  constructor(private service: EstudianteService) {
+    this.service.getStudents().subscribe(data => {
+      this.students = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data() as Estudiante
+        };
+      })
+    })
+  }
+
+  update(student: Estudiante, active: boolean) {
+    student.active = active;
+    this.service.updateStudent(student, student.id);
+  }
 }
